5a244ef86738058292b73adf9b73e9be3f2c31bd
Igor <igorlfs@ufmg.br>
2026-09-01 21:17:29 -0300

    Search for child sessions when handling source bufs
    Search for child sessions when handling source bufs

--- spec/integration_spec.lua
+++ spec/integration_spec.lua
@@ -824,6 +824,77 @@ describe('request source', function()
     assert.are.same({'foobar'}, lines)
   end)
 
+  it('can jump to frame if source needs to be fetched from nested session #only', function()
+    local srv2 = require('spec.server').spawn()
+    dap.adapters.dummy2 = srv2.adapter
+
+    api.nvim_create_autocmd("BufReadCmd", {
+      group = api.nvim_create_augroup("dap-readcmds", { clear = true }),
+      pattern = "dap-src://*",
+      ---@param args vim.api.keyset.create_autocmd.callback_args
+      callback = function(args)
+        require("dap._cmds").source(args.buf)
+      end,
+    })
+
+    run_and_wait_until_initialized(config, server)
+
+    server.client:send_request("startDebugging", {
+      request = "launch",
+      configuration = {
+        type = "dummy2",
+        name = "Subprocess"
+      }
+    })
+    wait(
+      function() return vim.tbl_count(dap.session().children) == 1 end,
+      function() return dap.session() end
+    )
+
+    srv2.client.source = function(self, request)
+      self:send_response(request, {
+        content = 'foobar',
+        mimeType = 'text/x-lldb.disassembly',
+      })
+    end
+    srv2.client.threads = function(self, request)
+      self:send_response(request, {
+        threads = { { id = 1, name = 'thread1' }, }
+      })
+    end
+    srv2.client.stackTrace = function(self, request)
+      self:send_response(request, {
+        stackFrames = {
+          {
+            id = 1,
+            name = 'stackFrame1',
+            line = 1,
+            column = 1,
+            source = {
+              sourceReference = 1
+            }
+          },
+        },
+      })
+    end
+    srv2.client:send_event('stopped', {
+      threadId = 1,
+      reason = 'breakpoint',
+    })
+    run_and_wait_until_initialized(config, srv2)
+    srv2.spy.clear()
+    wait_for_response(srv2, 'source')
+    assert.are.same('source', srv2.spy.responses[3].command)
+    assert.are.same('foobar', srv2.spy.responses[3].body.content)
+    wait(function()
+      return 'foobar' == api.nvim_buf_get_lines(0, 0, -1, false)[1]
+    end)
+    local lines = api.nvim_buf_get_lines(0, 0, -1, false)
+    assert.are.same({'foobar'}, lines)
+
+    srv2.stop()
+  end)
+
   it('sets filetype based on mimetype if available', function()
     server.client.source = function(self, request)
       self:send_response(request, {
